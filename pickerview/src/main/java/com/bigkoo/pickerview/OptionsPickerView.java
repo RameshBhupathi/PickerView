12554606013abf8877656287b66a5554adcd1182
Illuminati Diamond <davidxuxin@qq.com>
2018-01-25 13:28:11 +0800

    Add set button text method

--- pickerview/src/main/java/com/bigkoo/pickerview/OptionsPickerView.java
+++ pickerview/src/main/java/com/bigkoo/pickerview/OptionsPickerView.java
@@ -3,6 +3,7 @@ package com.bigkoo.pickerview;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.bigkoo.pickerview.view.BasePickerView;
@@ -15,18 +16,19 @@ import java.util.ArrayList;
  */
 public class OptionsPickerView<T> extends BasePickerView implements View.OnClickListener {
     WheelOptions wheelOptions;
-    private View btnSubmit, btnCancel;
+    private Button btnSubmit, btnCancel;
     private TextView tvTitle;
     private OnOptionsSelectListener optionsSelectListener;
     private static final String TAG_SUBMIT = "submit";
     private static final String TAG_CANCEL = "cancel";
+
     public OptionsPickerView(Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.pickerview_options, contentContainer);
         // -----确定和取消按钮
-        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
         btnSubmit.setTag(TAG_SUBMIT);
-        btnCancel = findViewById(R.id.btnCancel);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
         btnCancel.setTag(TAG_CANCEL);
         btnSubmit.setOnClickListener(this);
         btnCancel.setOnClickListener(this);
@@ -36,6 +38,21 @@ public class OptionsPickerView<T> extends BasePickerView implements View.OnClick
         final View optionspicker = findViewById(R.id.optionspicker);
         wheelOptions = new WheelOptions(optionspicker);
     }
+
+    /*
+    * 设置确定按钮标题
+    * */
+    public void setBtnSubmitText(String text) {
+        btnSubmit.setText(text);
+    }
+
+    /*
+    * 设置取消按钮标题
+    * */
+    public void setBtnCancelText(String text) {
+        btnCancel.setText(text);
+    }
+
     public void setPicker(ArrayList<T> optionsItems) {
         wheelOptions.setPicker(optionsItems, null, null, false);
     }
@@ -52,80 +69,90 @@ public class OptionsPickerView<T> extends BasePickerView implements View.OnClick
         wheelOptions.setPicker(options1Items, options2Items, options3Items,
                 linkage);
     }
+
     /**
      * 设置选中的item位置
+     *
      * @param option1
      */
-    public void setSelectOptions(int option1){
+    public void setSelectOptions(int option1) {
         wheelOptions.setCurrentItems(option1, 0, 0);
     }
+
     /**
      * 设置选中的item位置
+     *
      * @param option1
      * @param option2
      */
-    public void setSelectOptions(int option1, int option2){
+    public void setSelectOptions(int option1, int option2) {
         wheelOptions.setCurrentItems(option1, option2, 0);
     }
+
     /**
      * 设置选中的item位置
+     *
      * @param option1
      * @param option2
      * @param option3
      */
-    public void setSelectOptions(int option1, int option2, int option3){
+    public void setSelectOptions(int option1, int option2, int option3) {
         wheelOptions.setCurrentItems(option1, option2, option3);
     }
+
     /**
      * 设置选项的单位
+     *
      * @param label1
      */
-    public void setLabels(String label1){
+    public void setLabels(String label1) {
         wheelOptions.setLabels(label1, null, null);
     }
+
     /**
      * 设置选项的单位
+     *
      * @param label1
      * @param label2
      */
-    public void setLabels(String label1,String label2){
+    public void setLabels(String label1, String label2) {
         wheelOptions.setLabels(label1, label2, null);
     }
+
     /**
      * 设置选项的单位
+     *
      * @param label1
      * @param label2
      * @param label3
      */
-    public void setLabels(String label1,String label2,String label3){
+    public void setLabels(String label1, String label2, String label3) {
         wheelOptions.setLabels(label1, label2, label3);
     }
+
     /**
      * 设置是否循环滚动
+     *
      * @param cyclic
      */
-    public void setCyclic(boolean cyclic){
+    public void setCyclic(boolean cyclic) {
         wheelOptions.setCyclic(cyclic);
     }
-    public void setCyclic(boolean cyclic1,boolean cyclic2,boolean cyclic3) {
-        wheelOptions.setCyclic(cyclic1,cyclic2,cyclic3);
+
+    public void setCyclic(boolean cyclic1, boolean cyclic2, boolean cyclic3) {
+        wheelOptions.setCyclic(cyclic1, cyclic2, cyclic3);
     }
 
 
     @Override
-    public void onClick(View v)
-    {
-        String tag=(String) v.getTag();
-        if(tag.equals(TAG_CANCEL))
-        {
+    public void onClick(View v) {
+        String tag = (String) v.getTag();
+        if (tag.equals(TAG_CANCEL)) {
             dismiss();
             return;
-        }
-        else
-        {
-            if(optionsSelectListener!=null)
-            {
-                int[] optionsCurrentItems=wheelOptions.getCurrentItems();
+        } else {
+            if (optionsSelectListener != null) {
+                int[] optionsCurrentItems = wheelOptions.getCurrentItems();
                 optionsSelectListener.onOptionsSelect(optionsCurrentItems[0], optionsCurrentItems[1], optionsCurrentItems[2]);
             }
             dismiss();
@@ -142,7 +169,7 @@ public class OptionsPickerView<T> extends BasePickerView implements View.OnClick
         this.optionsSelectListener = optionsSelectListener;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         tvTitle.setText(title);
     }
 }
